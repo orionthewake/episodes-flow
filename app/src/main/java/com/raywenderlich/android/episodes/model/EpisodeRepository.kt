0e06d1abf1b82feebd5d19055e013f76604e85ca
Joseph Howard <joe.howard@mac.com>
2020-02-12 19:42:44 -0500

    Combine flows

--- app/src/main/java/com/raywenderlich/android/episodes/model/EpisodeRepository.kt
+++ app/src/main/java/com/raywenderlich/android/episodes/model/EpisodeRepository.kt
@@ -1,12 +1,13 @@
 package com.raywenderlich.android.episodes.model
 
+import androidx.annotation.AnyThread
 import com.raywenderlich.android.episodes.model.local.EpisodeDao
 import com.raywenderlich.android.episodes.model.network.EpisodeRemoteDataSource
+import com.raywenderlich.android.episodes.utils.ComparablePair
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.conflate
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -17,13 +18,26 @@ class EpisodeRepository @Inject constructor(
   private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
 ) {
 
+  private lateinit var favoritesSortOrder: List<String>
+
   val episodesFlow: Flow<List<Episode>>
     get() = episodeDao.loadAllEpisodesFlow()
+        .combine(favoritesFlow) { episodes, favoritesOrder ->
+          episodes.applySort(favoritesOrder)
+        }
         .flowOn(defaultDispatcher)
         .conflate()
 
+  private val favoritesFlow = flow {
+    favoritesSortOrder = episodeRDS.favoritesSortOrder()
+    emit(favoritesSortOrder)
+  }
+
   fun getEpisodesForTrilogyFlow(trilogy: Trilogy): Flow<List<Episode>> {
     return episodeDao.getEpisodesForTrilogyNumberFlow(trilogy.number)
+        .map { episodeList ->
+          episodeList.applyMainSafeSort(favoritesSortOrder)
+        }
   }
 
   private fun shouldUpdateEpisodesCache(): Boolean {
@@ -47,4 +61,19 @@ class EpisodeRepository @Inject constructor(
     val episodesForTrilogy = episodeRDS.episodesForTrilogy(trilogy)
     episodeDao.saveAll(episodesForTrilogy)
   }
+
+  private fun List<Episode>.applySort(favoritesSortOrder: List<String>): List<Episode> {
+    return sortedBy { episode ->
+      val positionForItem = favoritesSortOrder.indexOf(episode.episodeId).let { order ->
+        if (order > -1) order else Int.MAX_VALUE
+      }
+      ComparablePair(positionForItem, episode.number)
+    }
+  }
+
+  @AnyThread
+  suspend fun List<Episode>.applyMainSafeSort(favoritesSortOrder: List<String>) =
+      withContext(defaultDispatcher) {
+        this@applyMainSafeSort.applySort(favoritesSortOrder)
+      }
 }
