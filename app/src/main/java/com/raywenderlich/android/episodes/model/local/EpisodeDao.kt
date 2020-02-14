9c7de5182abb13c63beb886d8b741ed3c9cd62d2
Joe Howard <joe.howard@mac.com>
2020-02-14 15:06:48 -0500

    Add back in DB method

--- app/src/main/java/com/raywenderlich/android/episodes/model/local/EpisodeDao.kt
+++ app/src/main/java/com/raywenderlich/android/episodes/model/local/EpisodeDao.kt
@@ -3,7 +3,9 @@ package com.raywenderlich.android.episodes.model.local
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.raywenderlich.android.episodes.model.Episode
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface EpisodeDao {
@@ -12,4 +14,7 @@ interface EpisodeDao {
 
   @Insert(onConflict = OnConflictStrategy.REPLACE)
   suspend fun saveAll(episodes: List<Episode>)
+
+  @Query("SELECT * FROM episode ORDER BY number")
+  fun loadAllEpisodesFlow(): Flow<List<Episode>>
 }
